5eaac2ea010f7b3d9bc3a9f72005fda1d8d4c691
Senthuran Manoharan <senthuran.manoharan@gmail.com>
2020-07-13 12:58:34 +0530

    Displaying the JWT response

--- src/main/java/com/example/demo/filters/CustomCorsFilter.java
+++ src/main/java/com/example/demo/filters/CustomCorsFilter.java
@@ -11,8 +11,7 @@ import java.io.IOException;
 
 @Component
 public class CustomCorsFilter extends OncePerRequestFilter {
-
-
+    
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
--- src/main/resources/reactapp/frontend/reactapp/src/components/Signup.jsx
+++ src/main/resources/reactapp/frontend/reactapp/src/components/Signup.jsx
@@ -1,92 +1,125 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { form } from "react-bootstrap";
 import axios from 'axios';
 
 export default function Signup() {
 
     const [username, setUsername] = useState("");
-    const [password, setPassword] =useState("");
-    const [tokenusername,setTokenusername] = useState("");
-    const [tokenpassword,setTokenpassword] = useState ("");
-    const [jwt,setJwt] = useState ("");
+    const [password, setPassword] = useState("");
+    const [tokenusername, setTokenusername] = useState("");
+    const [tokenpassword, setTokenpassword] = useState("");
+    const [jwt, setJwt] = useState("");
+    const [jwtvalue, setJwtvalue] = useState("");
+    const [userresponse, setUserresponse] = useState("");
 
-    const handleSignUp = (e) =>{
+    const handleSignUp = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:8080/sign-up',{
-            username:username,
-            password:password})
-    };
-
-    const handleTokenRequest=(e)=>{
-        e.preventDefault()
-        axios.post('http://localhost:8080/token',{
-            username:tokenusername,
-            password:tokenpassword
+        axios.post('http://localhost:8080/sign-up', {
+            username: username,
+            password: password
         })
     };
 
-    const headers = {
-        'Authorization':'Bearer '+ jwt
-    }
-    const invokeUser=(e)=>{
+    const handleTokenRequest = (e) => {
         e.preventDefault()
-        axios.get('http://localhost:8080/user',{headers:headers})
+        axios.post('http://localhost:8080/token', {
+            username: tokenusername,
+            password: tokenpassword
+        })
+            .then((response) => {
+                setJwtvalue(response.data.jwt)
+                console.log(response.data.jwt);
+            }, (error) => {
+                console.log(error);
+            });
+    };
+
+    const headers = {
+        'Authorization': 'Bearer ' + jwt
+    }
+    const invokeUser = (e) => {
+        e.preventDefault()
+        axios.get('http://localhost:8080/user', { headers: headers })
+            .then((response) => {
+                setUserresponse(response.data);
+                console.log(response.data);
+            }, (error) => {
+                console.log(error);
+            })
     }
 
     return (
         <div>
             <div class="container">
-                <div class="card" >
-                    <div class="card-body">
-                        <h1>User Signup</h1>
-                        <form onSubmit={handleSignUp}>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Username</label>
-                                <input type="text" class="form-control" value={username} onChange={e => setUsername(e.target.value)} id="username" aria-describedby="emailHelp" placeholder="Enter Username" required/>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card" >
+                            <div class="card-body">
+                                <h1>User Signup</h1>
+                                <form onSubmit={handleSignUp}>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Username</label>
+                                        <input type="text" class="form-control" value={username} onChange={e => setUsername(e.target.value)} id="username" aria-describedby="emailHelp" placeholder="Enter Username" required />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input type="password" class="form-control" id="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter Password" required />
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">SignUP</button>
+                                </form>
                             </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter Password" required/>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card" >
+                            <div class="card-body">
+                                <h1>Request A JWT Token</h1>
+                                <form onSubmit={handleTokenRequest}>
+                                    <div class="form-group">
+                                        <label for="TokenUsername">Username</label>
+                                        <input type="text" class="form-control" value={tokenusername} onChange={e => setTokenusername(e.target.value)} id="tokenusername" aria-describedby="emailHelp" placeholder="Enter Username" required />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="TokenPassword">Password</label>
+                                        <input type="password" class="form-control" id="tokenpassword" value={tokenpassword} onChange={e => setTokenpassword(e.target.value)} placeholder="Enter Password" required />
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Request Token</button>
+                                </form>
+                                <div class="form-group">
+                                    <br />
+                                    <input type="text" class="form-control" id="jwt" value={jwtvalue} placeholder="Requested JWT" />
+                                </div>
                             </div>
-                            <button type="submit" class="btn btn-primary">SignUP</button>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <br/>
+            <br />
+
             <div class="container">
                 <div class="card" >
                     <div class="card-body">
-                        <h1>Request JWT Token</h1>
-                        <form onSubmit={handleTokenRequest}>
-                            <div class="form-group">
-                                <label for="TokenUsername">Username</label>
-                                <input type="text" class="form-control" value={tokenusername} onChange={e => setTokenusername(e.target.value)} id="tokenusername" aria-describedby="emailHelp" placeholder="Enter Username" required/>
-                            </div>
-                            <div class="form-group">
-                                <label for="TokenPassword">Password</label>
-                                <input type="password" class="form-control" id="tokenpassword" value={tokenpassword} onChange={e => setTokenpassword(e.target.value)} placeholder="Enter Password" required/>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Request Token</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <br/>
-            <div class="container">
-                <div class="card" >
-                    <div class="card-body">
-                        <h1>Invoke User Endpoint</h1>
+                        <h1>Invoke User/Hello Endpoint</h1>
                         <form onSubmit={invokeUser}>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Input JWT Token</label>
-                                <input type="text" class="form-control" value={jwt} onChange={e => setJwt(e.target.value)} id="jwt" aria-describedby="emailHelp" placeholder=" JWT " required/>
+                                <input type="text" class="form-control" value={jwt} onChange={e => setJwt(e.target.value)} id="jwt" aria-describedby="emailHelp" placeholder=" JWT " required />
                             </div>
-                            <button type="submit" class="btn btn-primary">Invoke User Endpoint</button>
+                            <div class="container">
+                                <div class="row">
+                                    <button type="submit" class="btn btn-primary">Invoke User Endpoint</button>
+                                    <button type="submit" class="btn btn-primary">Invoke Hello Endpoint</button>
+                                </div>
+                            </div>
                         </form>
+                        <div class="form-group">
+                            <br />
+                            <input type="text" class="form-control" id="userresponse" value={userresponse} placeholder=" Response " />
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
